57d5f16bd51f7af1acc7cf6c70c83096a6267c55
Hayotbahrom <hayotbahrom2002@gmail.com>
2026-08-10 19:03:40 +0500

    implemented: generic repository and asnotracking feature

---
+++ OrderManagement.Data/Reposiroty/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace OrderManagement.Data.Reposiroty;
+
+public interface IRepository<TEntity>
+{
+    Task<bool> DeleteAsync(int id);
+    IQueryable<TEntity> SelectAll();
+    Task<TEntity> SelectByIdAsync(int id);
+    Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}
